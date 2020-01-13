18bcdd2f509e41349a4a0084eeba2be88ebd87f5
blezalex <blezalex@gmail.com>
2020-01-12 17:52:46 -0800

    Fix time measurement bug

--- arduino.cpp
+++ arduino.cpp
@@ -10,10 +10,10 @@ void initArduino() {
 	TIM_TimeBaseInitTypeDef TimerBaseInit;
 	TIM_TimeBaseStructInit(&TimerBaseInit);
 
-	TimerBaseInit.TIM_Prescaler =  SystemCoreClock / 1000000000 - 1; // 1ms tick ;
+	TimerBaseInit.TIM_Prescaler =  SystemCoreClock / 2 / 1000 - 1; // 1ms tick ;
 	TimerBaseInit.TIM_Period = 0xFFFF;
 	TimerBaseInit.TIM_CounterMode = TIM_CounterMode_Up;
-	TimerBaseInit.TIM_ClockDivision = TIM_CKD_DIV1;
+	TimerBaseInit.TIM_ClockDivision = TIM_CKD_DIV2;
 	TIM_TimeBaseInit(MILLIS_TIMER,&TimerBaseInit);
 
 	TIM_Cmd(MILLIS_TIMER, ENABLE);
